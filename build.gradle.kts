3c95651282ce74e8224179d070599c8d770d3f45
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-11 08:17:24 +0300

    Initial MVP: Cluely keyboard with screenshot capture and AI vision chat

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.hilt) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.kotlin.ksp) apply false
+    kotlin(libs.plugins.kotlin.serialization.get().pluginId).version(libs.versions.kotlin).apply(false)
+}
